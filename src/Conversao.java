0404b014e2279b21862f0c663aa74fbc34aefe54
michael12345678 <michaelferreiradesousa@gmail.com>
2023-07-31 18:42:54 -0300

    first commit

---
+++ src/Conversao.java
@@ -0,0 +1,18 @@
+public class Conversao {
+
+    public static void main (String args[]){
+
+        int valor1 = 2348675;
+        System.out.println(valor1);
+
+        valor1 = Integer.valueOf(valor1);
+        System.out.println(valor1);
+
+        double valor1Double = 15.13;
+        System.out.println(valor1Double);
+
+        valor1Double = Double.valueOf(valor1Double);
+        System.out.println(valor1Double);
+
+    }
+}
